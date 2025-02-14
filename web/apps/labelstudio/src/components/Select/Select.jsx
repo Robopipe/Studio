941272b777c4dcfb9337505be4b6c20e3915693c
Adam Berkeš <adam.berkes@koala42.com>
2025-02-14 16:11:46 +0100

    Edit select for string options

--- web/apps/labelstudio/src/components/Select/Select.jsx
+++ web/apps/labelstudio/src/components/Select/Select.jsx
@@ -1,15 +1,21 @@
-import { Block, Elem } from "../../utils/bem"
-import "./Select.scss"
+import { Block, Elem } from "../../utils/bem";
+import "./Select.scss";
 
 export const Select = ({ label, options, onChange }) => {
-    return (
-        <Block tag="label" name="select">
-            <span>{label}</span>
-            <Block name="select" tag="select" onChange={(opt) => onChange(opt.target.value)}>
-                {options.map(option => (
-                    <Elem tag="option" key={option.value} value={option.value}>{option.label}</Elem>
-                ))}
-            </Block>
-        </Block>
-    )
-}
+  return (
+    <Block tag="label" name="select">
+      <span>{label}</span>
+      <Block
+        name="select"
+        tag="select"
+        onChange={opt => onChange(opt.target.value)}
+      >
+        {options.map(option => (
+          <Elem tag="option" key={option} value={option}>
+            {option}
+          </Elem>
+        ))}
+      </Block>
+    </Block>
+  );
+};
