09a3f505c8bf534dd420c53d8a45f91143818491
Adam Berkeš <adam.berkes@koala42.com>
2025-03-07 13:37:41 +0100

    Add detection inference

--- web/apps/labelstudio/src/pages/Infere/Infere.jsx
+++ web/apps/labelstudio/src/pages/Infere/Infere.jsx
@@ -1,14 +1,69 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { Select } from "../../components/Form";
+import { Input, Select } from "../../components/Form";
 import { Block, Elem } from "../../utils/bem";
 import "./Infere.scss";
 import { VideoPlayer } from "../../components/VideoPlayer/VideoPlayer";
 import { useCameraSelector } from "../../utils/camera-selector";
 import { useProject } from "../../providers/ProjectProvider";
 import { useAPI } from "../../providers/ApiProvider";
+import { useQuery } from "@tanstack/react-query";
+import { ca } from "date-fns/locale";
+import { useLabelConfig } from "../../utils/label-config";
+import { Button } from "../../components";
+import { ModelLogs } from "../../components/ModelDetail/ModelLogs";
+import { useLogs } from "../../hooks/useLogs";
+
+const classificationCb = (labels, elId, addLog) => ({ data }) => {
+  addLog?.(data);
+  const detections = JSON.parse(data).detections;
+  const el = document.getElementById(elId);
+  const maxDetectionIdx = detections.reduce(
+    (maxIdx, detection, idx) => (detection > detections[maxIdx] ? idx : maxIdx),
+    0
+  );
+  el.innerText = `Class: ${labels[maxDetectionIdx]}. Confidence: ${Math.floor(
+    detections[maxDetectionIdx] * 100
+  )}%`;
+};
+
+const detectionCb = (labels, elId, addLog) => {
+  const canvas = document.getElementById(elId);
+  const videoEl = document.getElementById("video-player");
+  const ctx = canvas.getContext("2d");
+  const colors = Array(labels.length)
+    .fill(0)
+    .map((_, i) => `hsl(${(i * 360) / labels.length}, 100%, 50%)`);
+
+  canvas.width = videoEl.clientWidth;
+  canvas.height = videoEl.clientHeight;
+  videoEl.addEventListener("resize", () => {
+    canvas.width = videoEl.clientWidth;
+    canvas.height = videoEl.clientHeight;
+  });
+
+  return ({ data }) => {
+    addLog?.(data);
+    const detections = JSON.parse(data).detections;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    detections.forEach(detection => {
+      const [xMin, yMin, xMax, yMax] = detection.coords;
+      const [x, y, w, h] = [xMin, yMin, xMax - xMin, yMax - yMin];
+      const [canvasW, canvasH] = [canvas.width, canvas.height];
+      ctx.strokeStyle = colors[detection.label];
+      ctx.strokeRect(x * canvasW, y * canvasH, w * canvasW, h * canvasH);
+      ctx.fillStyle = colors[detection.label];
+      ctx.fillText(
+        `${labels[detection.label]}: ${Math.floor(
+          detection.confidence * 100
+        )}%`,
+        x * canvasW,
+        y * canvasH - 5
+      );
+    });
+  };
+};
 
 export const InferePage = () => {
-  const apiUrl = "http://localhost:8080";
   const { project } = useProject();
   const api = useAPI();
   const {
@@ -19,92 +74,165 @@ export const InferePage = () => {
     wsUrl,
     camera,
     stream
-  } = useCameraSelector(apiUrl);
-  const [models, setModels] = useState([]);
+  } = useCameraSelector(true);
+  const { subscribe, unsubscribe, addLog } = useLogs();
+  const { data: models, status: modelsStatus } = useQuery({
+    queryKey: ["projects", project.id, "nn-models"],
+    queryFn: () => api.callApi("nnModels", { params: { pk: project.id } }),
+    enabled: !!project?.id
+  });
+  const { data: nnConfig, refetch: refetchNnConfig } = useQuery({
+    queryKey: ["cameras", camera, "streams", stream, "nn"],
+    queryFn: () =>
+      api.callApi("streamNn", { params: { mxid: camera, stream } }),
+    enabled: !!camera && !!stream
+  });
   const [model, setModel] = useState(null);
+  const [modelConfig, setModelConfig] = useState({ confidence: 0.5, iou: 0.5 });
   const [loading, setLoading] = useState(false);
-
+  const { numClasses, labelList } = useLabelConfig();
   const openInference = useCallback(() => {
+    if (!wsUrl || !nnConfig) return;
     const ws = new WebSocket(`${wsUrl}/nn`);
-    ws.onmessage = ({ data }) => {
-      const logEl = document.getElementById("inference-log");
-      const textEl = document.getElementById("inference-text");
-      logEl.textContent = data;
-      const detections = JSON.parse(data).detections;
-      const argMax = detections.reduce(
-        (acc, val, i, arr) => (val > arr[acc] ? i : acc),
-        0
-      );
-      textEl.textContent = `Class: ${
-        project.parsed_label_config.choice.labels[argMax]
-      }, confidence: ${Math.floor(detections[argMax] * 100)}%`;
-    };
-  }, [wsUrl]);
+    if (nnConfig.type === "Generic")
+      ws.onmessage = classificationCb(labelList, "inference-text", addLog);
+    else ws.onmessage = detectionCb(labelList, "inference-renderer", addLog);
+
+    return ws;
+  }, [wsUrl, nnConfig, labelList, addLog]);
 
   const uploadModel = useCallback(async () => {
     setLoading(true);
+    addLog("fetchiong model from backend");
     const modelBlob = await (
       await fetch(`http://localhost:8081/data/model/${model.model_path}`)
     ).blob();
     const formData = new FormData();
-    formData.append("model", modelBlob, model.model_path);
-    formData.append("nn_config", JSON.stringify({ type: "Generic" }));
+    const config = { type: model.model_type };
 
-    await fetch(`${apiUrl}/cameras/${camera}/streams/${stream}/nn`, {
-      method: "POST",
-      body: formData
-    });
-    openInference();
+    if (config.type === "YOLO") {
+      config.nn_config = {
+        num_classes: numClasses,
+        coordinate_size: 4,
+        confidence_threshold: modelConfig.confidence,
+        iou_threshold: modelConfig.iou
+      };
+    }
+
+    formData.append("model", modelBlob, model.model_path);
+    formData.append("config", JSON.stringify(config));
+    addLog(`uploading mode to camera ${camera} stream ${stream}`);
+    addLog(`model config: ${JSON.stringify(config)}`);
+
+    await fetch(
+      `http://localhost:8080/cameras/${camera}/streams/${stream}/nn`,
+      {
+        method: "POST",
+        body: formData
+      }
+    );
+    await refetchNnConfig();
     setLoading(false);
-  }, [model, camera, stream, openInference]);
+  }, [model, camera, stream, openInference, numClasses, addLog, modelConfig]);
+
+  const stopModel = useCallback(async () => {
+    setLoading(true);
+    await fetch(
+      `http://localhost:8080/cameras/${camera}/streams/${stream}/nn`,
+      {
+        method: "DELETE"
+      }
+    );
+    await refetchNnConfig();
+    setLoading(false);
+  }, [camera, stream, refetchNnConfig]);
 
   useEffect(() => {
-    if (!project?.id) return;
+    if (nnConfig) {
+      const ws = openInference();
 
-    api.callApi("nnModels", { params: { pk: project.id } }).then(data => {
-      setModels(data);
+      return () => {
+        ws.close();
+      };
+    }
+  }, [nnConfig, openInference]);
 
-      if (data.length > 0) setModel(data[0]);
-    });
-  }, [api, project]);
+  useEffect(() => {
+    if (models) setModel(models[0]);
+  }, [models]);
 
   return (
     <Block name="infere-page">
-      <Elem name="sidebar">
+      <Elem name="list-sidebar">
         <Elem name="settings">
           <Elem name="title">Settings</Elem>
+          {nnConfig && (
+            <Elem>
+              <Elem>Inference running</Elem>
+            </Elem>
+          )}
           <Select
-            options={cameras.map(cam => cam.mxid)}
+            options={(cameras ?? []).map(cam => cam.mxid)}
             onChange={e => setCamera(e.target.value)}
             label="Camera"
           />
           <Select
-            options={streams}
+            options={Object.keys(streams ?? {})}
             onChange={e => setStream(e.target.value)}
             label="Stream"
           />
           <Select
-            options={models.map(model => model.name)}
+            options={(models ?? []).map(model => model.name)}
             onChange={e =>
               setModel(models.find(model => model.name === e.target.value))
             }
             label="Model"
           />
-          <Elem
+          {model?.model_type === "YOLO" && (
+            <>
+              <Input
+                label="Confidence threshold"
+                type="number"
+                step={0.01}
+                defaultValue={0.5}
+                disabled={loading}
+              />
+              <Input
+                label="IoU threshold"
+                type="number"
+                step={0.01}
+                defaultValue={0.5}
+                disabled={loading}
+              />
+            </>
+          )}
+          <Button
             tag="button"
-            name="upload-btn"
             disabled={loading}
+            mod={{ primary: true }}
             onClick={uploadModel}
           >
             Upload
-          </Elem>
+          </Button>
+          <Button look="danger" disabled={loading} onClick={stopModel}>
+            Stop
+          </Button>
         </Elem>
       </Elem>
       <Elem name="content">
-        <VideoPlayer key={wsUrl} src={wsUrl && `${wsUrl}/video`} />
-        {/* <Elem tag="canvas" name="inference-renderer" id="inference-renderer" /> */}
-        <h2 id="inference-text"></h2>
-        <pre id="inference-log"></pre>
+        <Elem name="video-wrapper">
+          <VideoPlayer
+            key={wsUrl}
+            src={!loading && wsUrl && `${wsUrl}/video`}
+          />
+          <Elem
+            tag="canvas"
+            name="inference-renderer"
+            id="inference-renderer"
+          />
+          <Elem name="inference-text" id="inference-text" />
+        </Elem>
+        <ModelLogs subscribe={subscribe} unsubscribe={unsubscribe} />
       </Elem>
     </Block>
   );
