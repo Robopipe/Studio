325262ae7f1e404fdbe06917968e8b90730f76b7
Adam Berkeš <adam.berkes@koala42.com>
2026-01-30 17:54:51 +0100

    Update btn

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -1,82 +1,47 @@
-import { Button as BaseUIButton } from "@base-ui/react/button";
+import {
+  Button as BaseButton,
+  ButtonProps as BaseButtonProps,
+} from "@base-ui/react";
 import clsx from "clsx";
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import styles from "./Button.module.scss";
 
 export type ButtonVariant = "filled" | "outlined" | "text";
-export type ButtonSize = "sm" | "md" | "lg";
-export type ButtonColor = "primary" | "gray";
+export type ButtonSize = "xs" | "sm" | "md" | "lg";
 
-export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
+export interface ButtonProps extends BaseButtonProps {
   variant?: ButtonVariant;
   size?: ButtonSize;
-  color?: ButtonColor;
-  startIcon?: ReactNode;
-  endIcon?: ReactNode;
-  loading?: boolean;
   fullWidth?: boolean;
-  children?: ReactNode;
+  iconStart?: ReactNode;
+  iconEnd?: ReactNode;
 }
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (
-    {
-      className,
-      variant = "filled",
-      size = "md",
-      color = "primary",
-      startIcon,
-      endIcon,
-      loading = false,
-      fullWidth = false,
-      disabled = false,
-      children,
-      ...props
-    },
-    ref,
-  ) => {
-    const isDisabled = disabled || loading;
-
-    return (
-      <BaseUIButton
-        ref={ref}
-        disabled={isDisabled}
-        className={clsx(
-          styles.button,
-          styles[`button--${variant}`],
-          styles[`button--${size}`],
-          styles[`button--${color}`],
-          {
-            [styles["button--loading"]]: loading,
-            [styles["button--full-width"]]: fullWidth,
-          },
-          className,
-        )}
-        {...props}
-      >
-        {loading ? (
-          <>
-            <span className={styles["button__spinner"]} aria-hidden="true" />
-            {children && (
-              <span className={styles["button__text"]}>{children}</span>
-            )}
-          </>
-        ) : (
-          <>
-            {startIcon && (
-              <span className={styles["button__icon--start"]}>{startIcon}</span>
-            )}
-            {children && (
-              <span className={styles["button__text"]}>{children}</span>
-            )}
-            {endIcon && (
-              <span className={styles["button__icon--end"]}>{endIcon}</span>
-            )}
-          </>
-        )}
-      </BaseUIButton>
-    );
-  },
-);
-
-Button.displayName = "Button";
+export const Button = (props: ButtonProps) => {
+  const {
+    className,
+    children,
+    variant = "filled",
+    size = "md",
+    fullWidth,
+    iconStart,
+    iconEnd,
+    ...rest
+  } = props;
+  return (
+    <BaseButton
+      className={clsx(
+        styles.button,
+        styles[`button--${variant}`],
+        styles[`button--${size}`],
+        fullWidth && styles["button--fullWidth"],
+        className,
+      )}
+      {...rest}
+    >
+      {iconStart && <span className={styles.button__icon}>{iconStart}</span>}
+      {children}
+      {iconEnd && <span className={styles.button__icon}>{iconEnd}</span>}
+    </BaseButton>
+  );
+};
