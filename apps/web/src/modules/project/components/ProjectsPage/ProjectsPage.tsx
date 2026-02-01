443f2087d3adf8061674ade07a221759dc79cb38
Ivan Harasta <ivan@harasta.dev>
2026-02-01 02:12:23 +0100

    ✨ Project page

--- apps/web/src/modules/project/components/ProjectsPage/ProjectsPage.tsx
+++ apps/web/src/modules/project/components/ProjectsPage/ProjectsPage.tsx
@@ -1,4 +1,6 @@
+import { webRoutes } from "@/config/web/routes";
 import { AddSmallIcon, Button, Heading, Stack } from "@repo/ui";
+import { useNavigate } from "react-router";
 import { useActiveProject } from "../../hooks/useActiveProject";
 import { useGetProjectsQuery } from "../../services/projectApi";
 import { ProjectCard } from "../ProjectCard";
@@ -8,6 +10,7 @@ export interface ProjectsPageProps {}
 export const ProjectsPage = ({}: ProjectsPageProps) => {
   const { data: projects } = useGetProjectsQuery();
   const [activeProject, setActiveProject] = useActiveProject();
+  const navigate = useNavigate();
 
   return (
     <Stack fullWidth gap="md">
@@ -24,6 +27,9 @@ export const ProjectsPage = ({}: ProjectsPageProps) => {
           project={project}
           onClick={() => {
             setActiveProject(project);
+            navigate(
+              webRoutes.main.project.replace(":id", project.id.toString()),
+            );
           }}
         />
       ))}
