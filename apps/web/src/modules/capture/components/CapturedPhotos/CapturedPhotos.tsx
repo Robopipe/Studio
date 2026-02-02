a45f763700845f26de90abedd8993568f9e2cd5b
Adam Berkeš <adam.berkes@koala42.com>
2026-02-02 15:54:23 +0100

    Filter deleted tasks, allow sub second interval shooting

--- apps/web/src/modules/capture/components/CapturedPhotos/CapturedPhotos.tsx
+++ apps/web/src/modules/capture/components/CapturedPhotos/CapturedPhotos.tsx
@@ -33,7 +33,7 @@ export const CapturedPhotos = ({}: CapturedPhotosProps) => {
 
   return (
     <Stack fullWidth gap="md">
-      {tasks?.map((task) => (
+      {tasks?.filter(task => task.deletedAt === null).map((task) => (
         <div className={styles.photo} key={task.id}>
           <div className={styles.photoHeader}>
             <img src={task.filePath} alt={`#${task.id}`} />
