22ec22ce12ef4d27ecbde751d2e0fe8bef4dc3cc
Adam Berkeš <adam.berkes@koala42.com>
2026-02-09 16:21:51 +0100

    Implement types for model augmentation

--- packages/schema/src/model/model.types.ts
+++ packages/schema/src/model/model.types.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 import {
   createModelSchema,
+  modelAugmentationSchema,
   modelLogMetricsSchema,
   modelLogSchema,
   modelOutputSchema,
@@ -9,6 +10,7 @@ import {
 
 export type ModelLogMetrics = z.infer<typeof modelLogMetricsSchema>;
 export type ModelLog = z.infer<typeof modelLogSchema>;
+export type ModelAugmentation = z.infer<typeof modelAugmentationSchema>;
 export type Model = z.infer<typeof modelSchema>;
 export type CreateModel = z.infer<typeof createModelSchema>;
 export type ModelOutput = z.infer<typeof modelOutputSchema>;
