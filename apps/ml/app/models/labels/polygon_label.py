0251ef0e975046d8172496939fbd92ef35039be7
Adam Berkeš <adam.berkes@koala42.com>
2026-02-10 12:47:28 +0100

    Properly normalize labels

--- apps/ml/app/models/labels/polygon_label.py
+++ apps/ml/app/models/labels/polygon_label.py
@@ -5,12 +5,12 @@ from .label import Label
 
 class PolygonLabel(BaseSchema):
     label: Label
-    points: list[tuple[int, int]]
+    points: list[tuple[float, float]]
 
     def __get_normalized_points(
         self, width: int, height: int
     ) -> list[tuple[float, float]]:
-        return [(x / width, y / height) for x, y in self.points]
+        return [(x / 100, y / 100) for x, y in self.points]
 
     def to_str(self, width: int, height: int) -> str:
         normalized_points = self.__get_normalized_points(width, height)
