bb0bdbe4395608af4ac71fdda6ee09c63b625015
gandalfthegray <stranavadavid@protonmail.com>
2026-03-14 16:38:35 +0100

    Eval module

--- packages/database/src/schema/helpers/id.ts
+++ packages/database/src/schema/helpers/id.ts
@@ -1,3 +1,5 @@
-import { integer } from 'drizzle-orm/pg-core';
+import { integer, uuid, varchar } from 'drizzle-orm/pg-core';
+import {v7 as uuidv7} from 'uuid'
 
 export const id = integer().primaryKey().generatedAlwaysAsIdentity();
+export const uuidId = varchar('id', {length: 128}).primaryKey().$defaultFn(() => uuidv7()) // UUID7 supported natively in Postgres18+
