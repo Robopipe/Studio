7d75e32166c9ba8c192f5107e66f39ec4b19f98f
gandalfthegray <stranavadavid@protonmail.com>
2026-04-16 12:38:32 +0200

    Added sentry

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,23 +1,25 @@
 import { Module } from "@nestjs/common";
+import { APP_FILTER, APP_PIPE } from "@nestjs/core";
+import { SentryGlobalFilter, SentryModule } from "@sentry/nestjs/setup";
+import { ZodValidationPipe } from "nestjs-zod";
 import { ConfigurationModule } from "./core/configuration/configuration.module";
 import { DatabaseModule } from "./core/database/database.module";
 import { AuthModule } from "./modules/auth/auth.module";
+import { AssetsModule } from "./modules/assets/assets.module";
+import { CapturedVideoModule } from "./modules/captured-video/captured-video.module";
+import { DashboardModule } from "./modules/dashboard/dashboard.module";
+import { EmailModule } from "./modules/email/email.module";
+import { EvalModule } from "./modules/eval/eval.module";
+import { ModelModule } from "./modules/model/model.module";
 import { OrganizationModule } from "./modules/organization/organization.module";
 import { ProjectModule } from "./modules/project/project.module";
 import { RepositoryModule } from "./repository/repository.module";
-import { AssetsModule } from "./modules/assets/assets.module";
 import { TaskModule } from "./modules/task/task.module";
 import { TrainingExternalModule } from "./modules/training-external/training-external.module";
-import { ModelModule } from "./modules/model/model.module";
-import { DashboardModule } from "./modules/dashboard/dashboard.module";
-import { EmailModule } from "./modules/email/email.module";
-import { APP_PIPE } from "@nestjs/core";
-import { ZodValidationPipe } from "nestjs-zod";
-import { EvalModule } from "./modules/eval/eval.module";
-import { CapturedVideoModule } from "./modules/captured-video/captured-video.module";
 
 @Module({
   imports: [
+    SentryModule.forRoot(),
     AuthModule,
     ConfigurationModule,
     DatabaseModule,
@@ -35,9 +37,13 @@ import { CapturedVideoModule } from "./modules/captured-video/captured-video.mod
   ],
   providers: [
     {
+      provide: APP_FILTER,
+      useClass: SentryGlobalFilter,
+    },
+    {
       provide: APP_PIPE,
-      useClass: ZodValidationPipe
-    }
+      useClass: ZodValidationPipe,
+    },
   ]
 })
 export class AppModule {}
