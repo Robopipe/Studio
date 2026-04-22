a6dea35376cae700e77fde81cf99f3958ddd847f
David Stranava <david.stranava@koala42.com>
2026-04-22 14:57:59 +0200

    Fixed build

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,13 +2,14 @@ import "./instrument";
 
 import { VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 import { AppConfig } from "./core/configuration/app.config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: "1" });
 
