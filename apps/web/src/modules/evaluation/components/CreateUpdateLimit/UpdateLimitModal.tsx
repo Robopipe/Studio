fba20286ec7d7f50c6b87969fc0df812cb6780d6
Adam Berkeš <adam.berkes@koala42.com>
2026-05-26 14:28:56 +0200

    Edit limit modal UI

--- apps/web/src/modules/evaluation/components/CreateUpdateLimit/UpdateLimitModal.tsx
+++ apps/web/src/modules/evaluation/components/CreateUpdateLimit/UpdateLimitModal.tsx
@@ -45,7 +45,7 @@ export const UpdateLimitModal = ({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-h-[calc(100vh-4rem)] overflow-y-auto sm:max-w-3xl">
+      <DialogContent className="max-h-[calc(100vh-4rem)] overflow-y-auto sm:max-w-5xl">
         <DialogHeader>
           <DialogTitle className="text-xl font-semibold">Edit Limit</DialogTitle>
         </DialogHeader>
