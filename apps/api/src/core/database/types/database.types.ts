0304a8bcb37a4b3434962037434b5b882537fa0f
Adam Berkeš <adam.berkes@koala42.com>
2026-06-02 19:32:07 +0200

    Refactoring task services

--- apps/api/src/core/database/types/database.types.ts
+++ apps/api/src/core/database/types/database.types.ts
@@ -3,3 +3,4 @@ import * as schema from '@repo/database/schema';
 import relations from '@repo/database/schema/relations/index'
 
 export type DbConnection = NodePgDatabase<typeof schema, typeof relations>;
+export type DbTransaction = Parameters<Parameters<DbConnection['transaction']>[0]>[0];
--- apps/api/src/modules/task/task.module.ts
+++ apps/api/src/modules/task/task.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TaskService } from "./services/task.service";
+import { AnnotationService } from "./services/annotation.service";
 import { TaskController } from "./controllers/task.controller";
 import { AssetsModule } from "../assets/assets.module";
 
 @Module({
   imports: [AssetsModule],
-  providers: [TaskService],
+  providers: [TaskService, AnnotationService],
   controllers: [TaskController]
 })
 export class TaskModule{}
