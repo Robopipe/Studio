d175a2f8111dcd7a5989bc015066606058a55887
Adam Berkeš <adam.berkes@koala42.com>
2026-06-22 12:33:46 +0200

    split model and dashboard running state

--- apps/web/src/modules/reports/components/ReportsPage/ReportsPage.tsx
+++ apps/web/src/modules/reports/components/ReportsPage/ReportsPage.tsx
@@ -1,7 +1,7 @@
 import {
   useCreateReportMutation,
   useDeleteReportMutation,
-  useGetDashboardQuery,
+  useGetNNQuery,
   useListCamerasQuery,
   useListReportsQuery,
 } from "@/core/cameraApi";
@@ -31,7 +31,7 @@ export const ReportsPage = ({ dashboardId, projectId }: ReportsPageProps) => {
   });
   const { cameraMxid: selectedCamera, streamName: selectedStream } =
     useSelectedCameraStream(cameras);
-  const { isSuccess: isModelRunning } = useGetDashboardQuery(
+  const { data: isModelRunning } = useGetNNQuery(
     { mxid: selectedCamera!, streamName: selectedStream! },
     { skip: !selectedCamera || !selectedStream },
   );
